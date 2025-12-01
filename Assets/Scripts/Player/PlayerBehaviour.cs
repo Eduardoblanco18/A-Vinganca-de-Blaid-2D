26db55d2dff94368075a9916e1cd4810189d4fd6
Eduardo Domingues Blanco <eduardodominguesblanco@gmail.com>
2025-12-01 16:30:14 -0300

    Alterando velocidade

--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlayerBehavior : MonoBehaviour
 {
     private InputManager inputManager;
+    [SerializeField] private float moveSpeed = 5;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,7 +13,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     private void Update()
     {
-        float moveDirection = inputManager.Movement;
+        float moveDirection = inputManager.Movement * Time.deltaTime * moveSpeed;
         transform.Translate(moveDirection, 0, 0);
     }
 }
